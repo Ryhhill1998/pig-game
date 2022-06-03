40c37452ea81f2469adcf480fba1fe3c7a66d229
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-06-03 14:01:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ const p2CurrScoreEl = document.getElementById("current--1");
 const diceImgEl1 = document.querySelector(".dice--1");
 const diceImgEl2 = document.querySelector(".dice--2");
 
+const buttonsArray = document.querySelectorAll(".btn");
+const iconsArray = document.querySelectorAll(".icon");
+
 const newGameBtn = document.querySelector(".btn--new");
 const rollDiceBtn = document.querySelector(".btn--roll");
 const holdScoreBtn = document.querySelector(".btn--hold");
+const infoBtn = document.querySelector(".info");
+const closeModalBtn = document.querySelector(".close-modal");
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
 // --------------- GAME SETUP --------------- //
 
@@ -143,3 +151,68 @@ holdScoreBtn.addEventListener("click", function() {
 
 // add click event listener to new game button to restart game
 newGameBtn.addEventListener("click", resetGame);
+
+// --------------- GAME UI --------------- //
+
+// create increased margin animation between icon and button hovered
+for (let i = 0; i < buttonsArray.length; i++) {
+  const button = buttonsArray[i];
+  const icon = iconsArray[i];
+  button.addEventListener("mouseover", function() {
+    icon.classList.remove("button-unhovered");
+    icon.classList.add("button-hovered");
+  });
+  button.addEventListener("mouseout", function() {
+    icon.classList.remove("button-hovered");
+    icon.classList.add("button-unhovered");
+  });
+}
+
+// create rotate animation for rotate icon in roll dice button
+rollDiceBtn.addEventListener("mouseover", function() {
+  document.querySelector(".fa-rotate").classList.add("fa-rotate-90");
+});
+
+rollDiceBtn.addEventListener("mouseout", function() {
+  document.querySelector(".fa-rotate").classList.remove("fa-rotate-90");
+});
+
+// create animation for when info button is hovered over
+infoBtn.addEventListener("mouseover", function() {
+  this.classList.remove("info-unhovered");
+  this.classList.add("info-hovered");
+});
+
+infoBtn.addEventListener("mouseout", function() {
+  this.classList.remove("info-hovered");
+  this.classList.add("info-unhovered");
+});
+
+// --------------- MODAL WINDOW --------------- //
+
+// functions to show and close modal
+const showModal = function() {
+  if (modal.classList.contains("hidden")) {
+    overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+  }
+};
+
+const closeModal = function() {
+  if (!modal.classList.contains("hidden")) {
+    overlay.classList.add("hidden");
+    modal.classList.add("hidden");
+  };
+};
+
+infoBtn.addEventListener("click", showModal);
+
+closeModalBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
